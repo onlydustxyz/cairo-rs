4797c80111e3db948ef3ddd3f243c7df85864afa
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2023-02-01 09:39:54 +0100

    feat: introduce no_std by making std support a feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-#![deny(warnings)]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[macro_use]
+extern crate alloc;
+
+#[deny(warnings)]
 pub mod cairo_run;
 pub mod hint_processor;
 pub mod math_utils;
